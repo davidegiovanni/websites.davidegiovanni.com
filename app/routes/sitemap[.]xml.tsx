224b9e342a0018ffa6477356b3e2a9d8862d560a
Davide <33760151+davidegiovanni@users.noreply.github.com>
2023-08-29 23:03:00 +0200

    feat: add sitemap generations

--- app/routes/sitemap[.]xml.tsx
+++ app/routes/sitemap[.]xml.tsx
@@ -1,45 +1,78 @@
 import { LoaderFunction } from "@remix-run/node";
-import { safeGet } from "~/utils/safe-post";
+import { safeGetFeed } from "~/api";
+import { getSlug } from "~/utils/helpers";
+
+function getAllPagesUrl(host: string, pages: string[], lang: string): string[] {
+  const pagesUrl: string[] = []
+  pages.map(page => {
+    pagesUrl.push(`https://${host}/${lang}/${page}`)
+  })
+  return pagesUrl
+}
+
+async function getAllFeedsItemsUrl(host: string, feeds: { key: string; value: string; }[], lang: string): Promise<string[]> {
+  const itemsUrl: string[] = []
+  for (const feed of feeds) {
+    const feedRes = await safeGetFeed(feed.key, {})
+    if (typeof feedRes === "string" && feedRes === "error") {
+      itemsUrl.push(`https://${host}/${lang}/${feed.value}`)
+    } else if (typeof feedRes !== "string") {
+      const itemsSlugs: string[] = feedRes.items.map(item => getSlug(item.id) as string)
+      itemsUrl.push(...itemsSlugs.map(slug => `https://${host}/${lang}/${feed.value}/${slug}`))
+    }
+  }
+  return itemsUrl
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url)
 
   const host = (url.host.includes('localhost') || url.host.includes('192.168')) ? 'websites.davidegiovanni.com' : url.host
 
-  const pages: any = {
-    'it-IT': []
+  const allPages = [
+    {
+      locale: "it-IT",
+      pages: [],
+      feeds: [{
+        key: "websites-portfolio-it",
+        value: "websites"
+      }]
+    }
+  ]
+
+  let pagesUrl: string[] = []
+
+  async function processPages() {
+    for (const page of allPages) {
+      pagesUrl.push(`https://${host}/${page.locale}`);
+      
+      if (page.pages.length > 0) {
+        pagesUrl.push(...getAllPagesUrl(host, page.pages, page.locale));
+      }
+      
+      if (page.feeds.length > 0) {
+        const feedUrls = await getAllFeedsItemsUrl(host, page.feeds, page.locale);
+        pagesUrl.push(...feedUrls);
+      }
+    }
   }
   
-  const [defaultWebsiteRes, defaultWebsiteErr] = await safeGet<any>(request, `https://cdn.revas.app/websites/v0/websites/${host}?public_key=01exy3y9j9pdvyzhchkpj9vc5w`)
-  if (defaultWebsiteErr !== null) {
-    throw new Error(`Error: ${defaultWebsiteErr.message} ${defaultWebsiteErr.code}`);
-  }
+  await processPages();
 
-  const locales = defaultWebsiteRes.languageCodes
 
-const pagesList = locales.map((l: any) => (pages[l])?.map((p: any) =>
-`<url>
-  <loc>https://${host}/${l}/${p}</loc>
-</url>
-`).join("")
-)
-
-const indexesList = locales.map((l: any) =>
-`<url>
-  <loc>https://${host}/${l}</loc>
-</url>
-`).join("")
-  
-
-  const content = 
+const content =
 `<?xml version="1.0" encoding="UTF-8"?>
 <urlset
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
 >
-${indexesList}
-${pagesList}
+${pagesUrl.map(page =>
+`<url>
+  <loc>${page}</loc>
+</url>`
+).join("")
+}
 </urlset>
 `.trim()
 
